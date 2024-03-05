0131767f41980b762f611bc88e03e280d7ca22ea
poojakashyap-2920 <pooja337026@gmail.com>
2024-03-05 22:26:18 +0530

    TrafficSignle file added

---
+++ Traffic_Signle/TrafficLight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Signle
+{
+    internal class TrafficLight
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("****** TRAFFIC LIGHT CHANGES BASED ON TIME INTERVAL ******");
+            for (int i = 60; i > 20; i--)
+            {
+                Console.WriteLine(i + " " + "red");
+                Thread.Sleep(1000);
+
+
+
+            }
+            for (int j = 20; j > 1; j--)
+            {
+                Console.WriteLine(j + " " + "yello");
+                Thread.Sleep(1000);
+
+
+
+            }
+
+            for (int k = 1; k >= 0; k--)
+            {
+                Console.WriteLine(k + " " + "green");
+                Thread.Sleep(1000);
+
+
+
+            }
+
+        }
+
+    }
+}
